84e47833f675e5b73679283de88ac3aac8b52795
A2kmoise <abayomoise950@gmail.com>
2026-01-10 21:46:14 +0200

    -{ * * }- spring project configuration .yaml and .properties

--- src/main/java/org/codewithmoise/sping/OrderService.java
+++ src/main/java/org/codewithmoise/sping/OrderService.java
@@ -8,12 +8,12 @@ public class OrderService {
 
     private final PaymentService paymentService;
 
- public OrderService(@Qualifier("paypal") PaymentService paymentService){
+ public OrderService(@Qualifier("stripe") PaymentService paymentService){
        this.paymentService = paymentService;
    }
 
     public void placeOrder(){ // tight-coupled we can not test OrderService alone it should go with that Stripe payment which is not actually good it is solvable by using interfaces for dependency injection.
-    //var paymentService = new StripePaymentService(); (removed)
+    //var paymentService = new StripePaymentService (removed)
     paymentService.processPayment(100);
     }
 
--- src/main/java/org/codewithmoise/sping/StripePaymentService.java
+++ src/main/java/org/codewithmoise/sping/StripePaymentService.java
@@ -1,13 +1,32 @@
 package org.codewithmoise.sping;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Primary
 @Service("stripe")
 public class StripePaymentService implements PaymentService {
+
+    @Value("${stripe.apiUrl}")
+    private String appUrl;
+
+    @Value("${stripe.enabled}")
+    private boolean enabled;
+
+    @Value("${stripe.supported-currency}")
+    private List<String> currencies;
+
+    @Value("${stripe.timeout}")
+    private int timeout;
+
     @Override
     public void processPayment(double amount){
+        System.out.println("Host: "  + appUrl);
+        System.out.println("Enabled: " + enabled);
+        System.out.println("Currency: " + currencies);
         System.out.println("STRIPE");
         System.out.println("Amount: "+ amount + "paid");
     }

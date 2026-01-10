169de161a7c6c65ab4b864ad4bfd9857909bc5d8
A2kmoise <abayomoise950@gmail.com>
2026-01-10 15:42:21 +0200

    -{ * * }- email and order bean configuration

--- src/main/java/org/codewithmoise/sping/OrderService.java
+++ src/main/java/org/codewithmoise/sping/OrderService.java
@@ -17,8 +17,8 @@ public class OrderService {
     paymentService.processPayment(100);
     }
 
-    public void setPaymentService(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+//    public void setPaymentService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
 
 }
